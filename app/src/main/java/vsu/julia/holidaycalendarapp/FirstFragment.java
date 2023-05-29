5ae44b19d1111b8411e72f5fb6a436a7cb36713b
CezzarJ3 <silenia2014@gmail.com>
2023-05-29 22:33:56 +0300

    Updated main fragment, dropdown list for countries

--- app/src/main/java/vsu/julia/holidaycalendarapp/FirstFragment.java
+++ app/src/main/java/vsu/julia/holidaycalendarapp/FirstFragment.java
@@ -4,16 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import java.util.List;
+
 import vsu.julia.holidaycalendarapp.databinding.FragmentFirstBinding;
+import vsu.julia.holidaycalendarapp.sqldb.ISOCodesDbHandler;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+
+    private ISOCodesDbHandler isoCodesDbHandler;
 
     @Override
     public View onCreateView(
@@ -22,8 +28,17 @@ public class FirstFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+        isoCodesDbHandler = new ISOCodesDbHandler(getContext());
+        fillCountrySpinner();
         return binding.getRoot();
+    }
 
+    private void fillCountrySpinner() {
+        List<String> countries = isoCodesDbHandler.getAllCountries();
+        String[] arraySpinner = countries.toArray(new String[0]);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this.getContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, arraySpinner);
+        adapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        binding.countryInput.setAdapter(adapter);
     }
 
 //    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
